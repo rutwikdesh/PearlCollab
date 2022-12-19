88d30d8cbc296b000b3da6b2e0df1d51f4d36a7a
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2022-12-19 21:12:33 +0530

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAD-k1w1vi3l0ywGotie6xgiyS2Geob6dk",
+  authDomain: "pearlcollab-2a46d.firebaseapp.com",
+  projectId: "pearlcollab-2a46d",
+  storageBucket: "pearlcollab-2a46d.appspot.com",
+  messagingSenderId: "176682566378",
+  appId: "1:176682566378:web:979365ab7589220f604d93"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
