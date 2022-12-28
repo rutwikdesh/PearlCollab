6cdabe13807948def3c5af0048241aa1386fd5b3
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2022-12-29 00:14:02 +0530

    added dashboard pages base

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import './App.css'
 
 function App() {
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,10 @@
+import './Signup.css'
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div>
+        Signup
+    </div>
+  )
+}
