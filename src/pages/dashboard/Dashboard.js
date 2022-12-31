f8a0ba2e86be444f0073c3e8f93d960436312b40
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2022-12-31 15:28:58 +0530

    added dashboard code

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,16 @@
 import './Dashboard.css'
+import { useCollection } from '../../hooks/useCollection'
 import React from 'react'
+import ProjectList from '../../components/ProjectList'
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
   return (
     <div>
-        Dashboard
+      <h2 className="page-title">Dashboard</h2>
+      {error && <div className='error'>{error}</div>}
+      {documents && <ProjectList projects={documents} />}
     </div>
   )
 }
