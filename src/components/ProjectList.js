e24df2a2bfdd04c0712791c46ac44dca859cce4e
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2022-12-31 16:45:02 +0530

    changed indentation

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,24 +4,24 @@ import { Link } from 'react-router-dom'
 import Avatar from './Avatar'
 
 export default function ProjectList({ projects }) {
-    return (
-        <div className='project-list'>
-            {projects.length === 0 && <p>No projects yet!</p>}
-            {projects.map(project => (
-                <Link to={`/projects/${project.id}`} key={project.id}>
-                    <h4>{project.name}</h4>
-                    <p>Due by {project.dueDate.toDate().toString()}</p>
-                    <div className='assigned-to'>
-                        <ul>
-                            {project.assignedUsersList.map(user => (
-                                <li key={user.photoURL}>
-                                    <Avatar src={user.photoURL} />
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div className='project-list'>
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map(project => (
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toString()}</p>
+          <div className='assigned-to'>
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
 }
--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -2,28 +2,28 @@ import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config"
 
 export const useDocument = (collection, id) => {
-    const [document, setDocument] = useState(null)
-    const [error, setError] = useState(null)
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
 
-    useEffect(() => {
-        const ref = projectFirestore.collection(collection).doc(id)
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
 
-        const unsubscribe = ref.onSnapshot((snapshot) => {
-            if(snapshot.data()){
-                setDocument({ ...snapshot.data(), id: snapshot.id })
-                setError(null)
-            }
-            else{
-                setError('Document doesnot exist')
-            }
-        }, (err) => {
-            console.log(err.message)
-            setError('failed to fetch the requested document')
-        })
+    const unsubscribe = ref.onSnapshot((snapshot) => {
+      if (snapshot.data()) {
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+        setError(null)
+      }
+      else {
+        setError('Document doesnot exist')
+      }
+    }, (err) => {
+      console.log(err.message)
+      setError('failed to fetch the requested document')
+    })
 
-        return () => unsubscribe()
+    return () => unsubscribe()
 
-    }, [collection, id])
+  }, [collection, id])
 
-    return { document, error }
+  return { document, error }
 }
